72872168f2a31154e6d162058696f5506a89449d
WojtekWietrzny <wietrzny@student.agh.edu.pl>
2024-01-15 14:10:23 +0100

    hotfix

--- src/main/java/agh/ics/oop/model/Animal.java
+++ src/main/java/agh/ics/oop/model/Animal.java
@@ -1,4 +1,4 @@
-﻿package agh.ics.oop.model;
+package agh.ics.oop.model;
 
 import agh.ics.oop.model.enums.MapDirection;
 
--- src/main/java/agh/ics/oop/model/MapCell.java
+++ src/main/java/agh/ics/oop/model/MapCell.java
@@ -1,4 +1,4 @@
-﻿package agh.ics.oop.model;
+package agh.ics.oop.model;
 
 import java.util.ArrayList;
 
